045452169e80574fe011f6cb2cdfc4d4b65a2d7f
Vishal Koshti <koshtivishal89@gmail.com>
2020-06-17 11:35:48 +0530

    Added project Learning 01

---
+++ Learning01/src/main/java/com/mastek/Learning01/model/Product.java
@@ -0,0 +1,30 @@
+package com.mastek.Learning01.model;
+
+public class Product {
+
+	private String productId;
+	private String productName;
+	private double price;
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + "]";
+	}
+}
