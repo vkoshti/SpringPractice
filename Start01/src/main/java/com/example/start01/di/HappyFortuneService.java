febb5bf5a1d6f0d7b437823e5c4fe5b33bcaebee
vkoshti <vishalkoshti165@gmail.com>
2021-04-04 00:57:12 +0530

    Added random fortunes, change in file HappyFortuneService.java

--- Start01/src/main/java/com/example/start01/di/HappyFortuneService.java
+++ Start01/src/main/java/com/example/start01/di/HappyFortuneService.java
@@ -1,5 +1,7 @@
 package com.example.start01.di;
 
+import java.util.Random;
+
 import com.example.start01.di.definition.FortuneService;
 
 /**
@@ -8,10 +10,18 @@ import com.example.start01.di.definition.FortuneService;
  *
  */
 public class HappyFortuneService implements FortuneService {
+	
+	private String[] fortunes = {
+			"You're going to rock today",
+			"You will hear some good news",
+			"This is your day"
+	};
 
 	@Override
 	public String getDailyFortune() {
-		return "You will hear a good news today";
+		Random random = new Random();
+		int index = random.nextInt(fortunes.length);
+		return fortunes[index];
 	}
 
 }
